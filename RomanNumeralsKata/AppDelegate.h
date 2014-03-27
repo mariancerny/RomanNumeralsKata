d47d8811b5064dd83536661b2bf2477aef98503a
Marián Černý <majo@Marians-MacBook-Air.local>
2014-03-27 22:43:31 +0100

    Empty project.

---
+++ RomanNumeralsKata/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RomanNumeralsKata
+//
+//  Created by Marián Černý on 27.03.14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RomanNumeralsKataTests/RomanNumeralsKataTests.m
@@ -0,0 +1,34 @@
+//
+//  RomanNumeralsKataTests.m
+//  RomanNumeralsKataTests
+//
+//  Created by Marián Černý on 27.03.14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RomanNumeralsKataTests : XCTestCase
+
+@end
+
+@implementation RomanNumeralsKataTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
