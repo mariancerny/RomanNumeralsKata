7d41cc3eae28c14eea69e98cb11543c3637ec169
Marián Černý <majo-github@cerny.sk>
2014-04-27 05:59:51 +0200

    Added test for number 1.

--- RomanNumeralsKataTests/RomanNumeralsKata2Tests.m
+++ RomanNumeralsKataTests/RomanNumeralsKata2Tests.m
@@ -26,12 +26,24 @@
     XCTAssertEqualObjects(romanNumber, @"", @"Zero should convert to an empty string");
 }
 
+- (void)testOneConvertsToI
+{
+    NSNumber *arabicNumber = @1;
+
+    NSString *romanNumber = [arabicNumber toRoman];
+
+    XCTAssertEqualObjects(romanNumber, @"I", @"One should convert to I");
+}
+
 @end
 
 @implementation NSNumber (RomanNumeral)
 
 - (NSString *)toRoman
 {
+    if ([self isEqualToNumber:@1])
+        return @"I";
+    
     return @"";
 }
 
